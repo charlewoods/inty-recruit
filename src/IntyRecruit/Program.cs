75a5690d5499372d400c676610c474966ff2c00d
charlewoods <steve.charlewood@gmail.com>
2016-11-29 21:56:40 +0000

    .Net Core commit
    Added the application put together to replicate the actions I performed
    to find the apply location.

---
+++ src/IntyRecruit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace IntyRecruit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceManipulator = new RecruitmentServiceManipulator();
+            Console.WriteLine("Starting process");
+            serviceManipulator.PerformRecruitmentSteps();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/IntyRecruit/RecruitmentServiceManipulator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace IntyRecruit
+{
+    public class RecruitmentServiceManipulator
+    {
+        private static readonly HttpClient Client = new HttpClient();
+
+        public void PerformRecruitmentSteps()
+        {
+            var optionsUri = new Uri("https://recruitment.inty.com/api/options");
+            var applyUri = new Uri("https://recruitment.inty.com/api/apply");
+            var applyWithUrlUri = new Uri(applyUri.ToString() + "?url=https://digitalshrubbey.azurewebsites.net/api/greetings");
+            makeRequestAndWriteResponse(optionsUri, HttpMethod.Get);
+            makeRequestAndWriteResponse(optionsUri, HttpMethod.Options);
+            makeRequestAndWriteResponse(optionsUri, HttpMethod.Options, "en-AU");
+            makeRequestAndWriteResponse(applyUri, HttpMethod.Get);
+            makeRequestAndWriteResponse(applyWithUrlUri, HttpMethod.Post);
+            makeRequestAndWriteResponse(new Uri("https://recruitment.inty.com/api/submit"),HttpMethod.Get);
+
+        }
+
+        private static void makeRequestAndWriteResponse(Uri uri, HttpMethod method, string language = null)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = uri,
+            };
+            if (language != null)
+            {
+                request.Headers.Add("Accept-language", language);
+            }
+
+            var result = Client.SendAsync(request);
+
+            Console.WriteLine(result.Result.ToString());
+            Console.WriteLine(result.Result.Content.ReadAsStringAsync().Result);
+
+        }
+
+       
+    }
+}
